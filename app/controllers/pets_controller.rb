480420e8eab90fa4a6b181d9583dd7fbe355dcf7
Lauren McCoy <lmccoy8911@gmail.com>
2019-10-14 16:00:44 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -32,13 +32,10 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do 
-#binding.pry 
    if !params[:pet].keys.include?("owner_id")
     params[:pet]["owner_id"] = []
   end 
-   
   @pet = Pet.find(params[:id])
-  #binding.pry 
   @pet.update(params["pet"])
   if !params["owner"]["name"].empty?
     @pet.owner = Owner.create(name: params["owner"]["name"])
